6e3ab1f80667805e36d2a128dd532138d0be1a0a
simon jimenez <simoncho2002@gmail.com>
2024-11-02 16:37:47 -0300

    garagescontrollerviews

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   resources :users
+  resources :garages
 
   # Defines the root path route ("/")
   # root "posts#index"
